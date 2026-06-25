316d8ebab244b1d74102cd7a913047a7fc34b926
xinxiangjueming <kongjunhuai@gmail.com>
2026-06-25 21:05:41 +0800

    增加隐藏软件、音频播放功能

--- app/src/main/java/com/example/fold/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/fold/ui/navigation/NavGraph.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.fold.MainActivity
 import com.example.fold.ui.filebrowser.FileBrowserScreen
 import com.example.fold.ui.calculator.CalculatorScreen
 import com.example.fold.ui.hidden.HiddenAppsScreen
